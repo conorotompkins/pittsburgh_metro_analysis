6a1fdc1bf96d84ae4a7a328e574a383bf7787067
Conor Tompkins <conorotompkins@gmail.com>
2020-08-10 18:08:19 -0400

    got cross-state connections working (really this time)

--- scripts/map_lodes_network_connections.R
+++ scripts/map_lodes_network_connections.R
@@ -11,28 +11,54 @@ options(tigris_use_cache = TRUE,
         scipen = 999,
         digits = 4)
 
-pa_lodes_od <- grab_lodes(state = "pa", year = 2010, 
+lodes_od_pa_main <- grab_lodes(state = "pa", year = 2010, 
                                  lodes_type = "od", job_type = "JT00", 
                                  segment = "S000", state_part = "main", 
                                  agg_geo = "county") %>% 
   select(state, w_county, h_county, S000, year) %>% 
   rename(commuters = S000)
 
-ny_lodes_od <- grab_lodes(state = "ny", year = 2010, 
+lodes_od_pa_aux <- grab_lodes(state = "pa", year = 2010, 
+                               lodes_type = "od", job_type = "JT00", 
+                               segment = "S000", state_part = "aux", 
+                               agg_geo = "county") %>% 
+  select(state, w_county, h_county, S000, year) %>% 
+  rename(commuters = S000)
+
+lodes_od_ny_main <- grab_lodes(state = "ny", year = 2010, 
                           lodes_type = "od", job_type = "JT00", 
                           segment = "S000", state_part = "main", 
                           agg_geo = "county") %>% 
   select(state, w_county, h_county, S000, year) %>% 
   rename(commuters = S000)
 
-nj_lodes_od <- grab_lodes(state = "nj", year = 2010, 
+lodes_od_ny_aux <- grab_lodes(state = "ny", year = 2010, 
+                               lodes_type = "od", job_type = "JT00", 
+                               segment = "S000", state_part = "aux", 
+                               agg_geo = "county") %>% 
+  select(state, w_county, h_county, S000, year) %>% 
+  rename(commuters = S000)
+
+lodes_od_nj_main <- grab_lodes(state = "nj", year = 2010, 
                           lodes_type = "od", job_type = "JT00", 
                           segment = "S000", state_part = "main", 
                           agg_geo = "county") %>% 
   select(state, w_county, h_county, S000, year) %>% 
   rename(commuters = S000)
 
-lodes_combined <- bind_rows(pa_lodes_od, ny_lodes_od, nj_lodes_od)
+lodes_od_nj_aux <- grab_lodes(state = "nj", year = 2010, 
+                               lodes_type = "od", job_type = "JT00", 
+                               segment = "S000", state_part = "aux", 
+                               agg_geo = "county") %>% 
+  select(state, w_county, h_county, S000, year) %>% 
+  rename(commuters = S000)
+
+lodes_combined <- bind_rows(lodes_od_pa_main, lodes_od_pa_aux,
+                            lodes_od_ny_main, lodes_od_ny_aux,
+                            lodes_od_nj_main, lodes_od_nj_aux)
+
+lodes_combined %>% 
+  count(h_county, w_county, sort = TRUE)
 
 # pa_lodes_od %>% 
 #   rename(workers = S000) %>% 
@@ -54,25 +80,30 @@ lodes_combined <- bind_rows(pa_lodes_od, ny_lodes_od, nj_lodes_od)
 
 
 pa_counties <- tigris::counties(state = "PA") %>% 
-  select(GEOID, NAME) %>% 
+  mutate(state = "PA") %>%
+  select(state, NAME, GEOID) %>% 
   arrange(GEOID)
 
 ny_counties <- tigris::counties(state = "NY") %>% 
-  select(GEOID, NAME) %>% 
+  mutate(state = "NY") %>%
+  select(state, NAME, GEOID) %>%  
   arrange(GEOID)
 
 nj_counties <- tigris::counties(state = "NJ") %>% 
-  select(GEOID, NAME) %>% 
+  mutate(state = "NJ") %>%
+  select(state, NAME, GEOID) %>% 
   arrange(GEOID)
 
 counties_combined <- bind_rows(pa_counties, ny_counties, nj_counties)
 
 states <- counties_combined %>% 
-  group_by()
+  group_by(state) %>% 
+  summarize()
 
 counties_combined %>% 
   ggplot() +
-  geom_sf()
+  geom_sf() +
+  geom_sf(data = states, fill = NA, color = "red")
 
 node_pos <- counties_combined %>% 
   mutate(centroid = map(geometry, st_centroid),
@@ -81,51 +112,61 @@ node_pos <- counties_combined %>%
   select(GEOID, NAME, x, y) %>% 
   st_drop_geometry() %>% 
   as_tibble() %>% 
-  clean_names() %>% 
-  select(-c(name, geoid))
+  select(-NAME) %>% 
+  arrange(GEOID)
 
+node_pos %>% 
+  filter(GEOID == "34001")
 
-
-network_graph <- lodes_combined %>% 
-  select(w_county, h_county, commuters) %>% 
+network_graph <- lodes_combined %>%
+  semi_join(counties_combined, by = c("w_county" = "GEOID")) %>%
+  semi_join(counties_combined, by = c("h_county" = "GEOID")) %>%
+  select(h_county, w_county, commuters) %>% 
   as_tbl_graph(directed = TRUE) %>% 
   # activate(nodes) %>% 
   # mutate(community1 = as.factor(group_infomap()),
   #        community2 = as.factor(group_edge_betweenness(directed = TRUE)),
   #        community2 = fct_lump_min(community2, 2)) %>% 
   activate(edges) %>% 
-  filter(commuters > 1000,
-         !edge_is_loop())
+  filter(commuters >= 5000,
+         !edge_is_loop()) %>%
+  activate(nodes) %>%
+  arrange(name)
+
+nodes <- network_graph %>%
+  activate(nodes) %>% 
+  as_tibble()
+
+all(node_pos$GEOID == nodes$name)
+
+identical(node_pos$GEOID, nodes$name)
+
+length(node_pos$GEOID) == length(node_pos$GEOID)
 
 network_graph %>% 
   activate(edges) %>% 
   arrange(desc(commuters)) %>% 
   as_tibble()
 
-network_graph %>%
-  activate(nodes) %>% 
-  as_tibble() %>% 
-  mutate(community2 = fct_lump_min(community2, 2)) %>% 
-  count(community2, sort = TRUE)
-
-
-#geo_layout <- layout_to_table(graph = network_graph, node_pos)
-
 geo_layout <- create_layout(graph = network_graph,
                             layout = node_pos)
 
 plot <- ggraph(geo_layout) +
-  geom_sf(data = counties_combined, fill = NA) +
+  geom_sf(data = counties_combined, fill = NA, 
+          #color = NA, 
+          size = .1) +
+  geom_sf(data = states, aes(color = state), fill = NA, size = 2) +
   geom_edge_fan(aes(edge_width = commuters, edge_alpha = commuters),
                 arrow = arrow(length = unit(4, 'mm')), 
                 start_cap = circle(3, 'mm'),
                 end_cap = circle(6, 'mm'),
-                color = "black") +
+                color = "white") +
   geom_node_point(color = "red", size = 3) +
   #geom_node_point(aes(color = community2)) +
-  scale_edge_alpha(range = c(.1, .9)) +
+  scale_edge_alpha(range = c(.2, .8)) +
   scale_edge_width(range = c(1, 7)) +
-  theme_void()
+  theme_void() +
+  theme(plot.background = element_rect(fill = "black"))
 
 plot %>% 
   ggsave(filename = "output/lodes_network_graph_combined.png", width = 24, height = 24, dpi = 300)
