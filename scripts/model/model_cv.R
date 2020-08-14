f2ce51b5470febe156c0adc7116e27566f11e10e
Conor Tompkins <conorotompkins@gmail.com>
2020-08-14 11:03:36 -0400

    updated with renamed density variable, added roc and vi graphs, mapped results

--- scripts/model/model_cv.R
+++ scripts/model/model_cv.R
@@ -23,8 +23,7 @@ tracts <- get_decennial(year = 2010, state = "PA", county = "Allegheny County",
                         geography = "tract", geometry = TRUE)
 
 city_tracts <- read_csv("data/selected_tracts.csv", col_types = cols("c", "l")) %>% 
-  filter(selected == TRUE) %>% 
-  rename(GEOID = id)
+  filter(selected == TRUE)
 
 glimpse(city_tracts)
 
@@ -36,20 +35,21 @@ pgh_official_boundary <- st_read("data/Pittsburgh_City_Boundary-shp") %>%
 
 tracts %>% 
   left_join(city_tracts) %>% 
+  mutate(type = case_when(selected == TRUE ~ "city",
+                          is.na(selected) ~ "non_city")) %>% 
   ggplot() +
-  geom_sf(aes(fill = selected)) +
+  geom_sf(aes(fill = type), size = .1) +
   geom_sf(data = pgh_official_boundary, color = "white", linetype = 2, alpha = 0) +
   theme_void()
-
 
 census_combined <- read_csv("data/combined_census_data_tract.csv", col_types = cols(.default = "c")) %>%
   left_join(city_tracts) %>% 
   mutate(type = case_when(selected == TRUE ~ "city",
                           is.na(selected) ~ "non_city")) %>% 
-  mutate(across(pct_units_owned_loan:housed_population_density_1k_per_km, as.numeric)) %>% 
+  mutate(across(pct_units_owned_loan:housed_population_density_pop_per_km, as.numeric)) %>% 
   select(GEOID, type, everything()) %>%
   mutate(flag_void = (total_population == 0 &
-                        housed_population_density_1k_per_km == 0 &
+                        housed_population_density_pop_per_km == 0 &
                         jobs == 0 &
                         workers == 0) %>% as.factor) %>% 
   select(-c(flag_void, selected, total_population, total_population_housed, pct_asian, pct_hispanic))
@@ -89,11 +89,6 @@ lm_workflow <- workflow() %>%
   add_recipe(model_recipe_prep) %>% 
   add_model(lm_model)
 
-lm_workflow %>% 
-  fit(training) %>% 
-  pull_workflow_fit()
-
-
 lm_res <- lm_workflow %>% 
   fit_resamples(resamples = validation) %>% 
   mutate(model = "lm")
@@ -101,17 +96,7 @@ lm_res <- lm_workflow %>%
 lm_res %>% 
   collect_metrics()
 
-lm_res %>% 
-  unnest(.metrics) %>% 
-  select(id, .metric, .estimate) %>% 
-  View()
-  #mutate(id = str_extract(id, "[:digit:]+") %>% parse_number) %>% 
-  ggplot(aes(.estimate, color = .metric, fill = .metric)) +
-  geom_density() +
-  facet_wrap(~.metric, scales = "free_y")
-  
-
-
+#rf
 rf_workflow <- workflow() %>% 
   add_recipe(model_recipe_prep) %>% 
   add_model(ranger_model)
@@ -122,13 +107,6 @@ rf_res <- rf_workflow %>%
 
 rf_res %>% 
   collect_metrics()
-
-rf_res %>% 
-  unnest(.metrics) %>% 
-  #mutate(id = str_extract(id, "[:digit:]+") %>% parse_number) %>% 
-  ggplot(aes(.estimate, color = .metric, fill = .metric)) +
-  geom_density() +
-  facet_wrap(~.metric, scales = "free_y")
 
 combined_res <- bind_rows(rf_res, lm_res)
 
@@ -154,6 +132,28 @@ predictions_training %>%
 predictions_training %>% 
   conf_mat(truth = type, estimate = .pred_class)
 
+#roc
+df_roc_curve <- fit(rf_workflow, juice(model_recipe_prep)) %>% 
+  predict(juice(model_recipe_prep), type = "prob") %>% 
+  bind_cols(juice(model_recipe_prep)) %>% 
+  mutate(type = as.factor(type)) %>% 
+  roc_curve(truth = type, .pred_city)
+
+df_roc_curve %>% 
+  autoplot()
+
+#variable importance
+var_imp <- rf_workflow %>% 
+  fit(juice(model_recipe_prep)) %>% 
+  pull_workflow_fit() %>% 
+  vip::vi()
+
+var_imp %>%
+  mutate(Variable = fct_reorder(Variable, Importance)) %>% 
+  ggplot(aes(Importance, Variable)) +
+  geom_point() +
+  theme_bw()
+
 ### predict on testing
 predictions_testing <- fit(rf_workflow, bake(model_recipe_prep, testing)) %>% 
   predict(bake(model_recipe_prep, testing)) %>% 
@@ -167,3 +167,59 @@ predictions_testing %>%
 #failures are false positives this time
 predictions_testing %>% 
   conf_mat(truth = type, estimate = .pred_class)
+
+
+#map results
+
+#predict on full dataset
+#probabilities
+full_predictions <- fit(rf_workflow, bake(model_recipe_prep, census_combined)) %>% 
+  predict(bake(model_recipe_prep, census_combined), type = "prob") %>% 
+  bind_cols(bake(model_recipe_prep, census_combined)) %>% 
+  mutate(type = as.factor(type))
+
+full_predictions %>%
+  write_csv("output/full_prediction_percent.csv")
+
+full_predictions_small <- full_predictions %>% 
+  select(GEOID, type, contains(".pred")) %>% 
+  pivot_longer(cols = contains(".pred"))
+
+prediction_pct_map <- tracts %>% 
+  select(GEOID) %>% 
+  left_join(full_predictions_small, by = "GEOID") %>% 
+  mutate(name = case_when(name == ".pred_city" ~ "predicted_city",
+                          name == ".pred_non_city" ~ "predicted_non_city")) %>% 
+  filter(!is.na(name)) %>% 
+  ggplot() +
+  geom_sf(aes(fill = value), color = NA) +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "black") +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "yellow", linetype = 2) +
+  facet_wrap(~name) +
+  labs(fill = "Percent") +
+  scale_fill_viridis_c() +
+  theme_void()
+
+prediction_pct_map
+
+#binary predictions
+full_predictions_binary <- fit(rf_workflow, bake(model_recipe_prep, census_combined)) %>% 
+  predict(bake(model_recipe_prep, census_combined)) %>% 
+  bind_cols(bake(model_recipe_prep, census_combined)) %>% 
+  mutate(type = as.factor(type),
+         correct = type == .pred_class)
+
+full_predictions_binary_small <- full_predictions_binary %>% 
+  select(GEOID, type, .pred_class, correct)
+
+prediction_binary_map <- tracts %>% 
+  select(GEOID) %>% 
+  left_join(full_predictions_binary_small, by = "GEOID") %>% 
+  ggplot() +
+  geom_sf(aes(fill = correct), color = NA, alpha = .7) +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "black") +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "yellow", linetype = 2) +
+  scale_fill_viridis_d() +
+  theme_void()
+
+prediction_binary_map
