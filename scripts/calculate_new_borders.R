e3293c415e67a8a3138f12e928cbb4f88368b4e8
Conor Tompkins <conorotompkins@gmail.com>
2020-08-26 14:55:29 -0400

    updated border creation script to adjust for new model predictions

--- scripts/calculate_new_borders.R
+++ scripts/calculate_new_borders.R
@@ -11,14 +11,14 @@ full_predictions <- read_csv("output/full_prediction_percent.csv",
 
 tract_info <- read_csv("data/combined_census_data_tract.csv", 
                        col_types = cols(.default = "c")) %>% 
-  mutate(across(total_population_housed:housed_population_density_1k_per_km, as.numeric)) %>% 
+  mutate(across(total_population_housed:housed_population_density_pop_per_square_km, as.numeric)) %>% 
   mutate(across(where(is.numeric), round, digits = 2))
 
 full_predictions_small <- full_predictions %>% 
-  select(GEOID, contains(".pred")) %>% 
+  select(GEOID, mean_city) %>% 
   mutate(GEOID = as.character(GEOID),
-         across(contains(".pred"), as.numeric),
-         across(contains(".pred"), round, digits = 2)) %>% 
+         mean_city = as.double(mean_city),
+         mean_city = round(mean_city, 2)) %>% 
   left_join(tract_info)
 
 glimpse(full_predictions_small)
@@ -38,14 +38,24 @@ pgh_official_boundary <- st_read("data/Pittsburgh_City_Boundary-shp") %>%
 tract_pred <- tracts %>% 
   left_join(full_predictions_small)
 
+tract_pred %>% 
+  ggplot(aes(mean_city)) +
+  geom_histogram()
+
+tract_pred %>% 
+  ggplot(aes(mean_city)) +
+  stat_ecdf(geom = "step")
+
 threshhold_map <- seq(.1, .5, by = .01) %>% 
   set_names() %>% 
   map_df(~mutate(tracts %>% 
-        left_join(full_predictions_small), flag_city = .pred_city >= .x), .id = "threshhold") %>%
+        left_join(full_predictions_small), flag_city = mean_city >= .x), .id = "threshhold") %>%
   arrange(desc(threshhold)) %>% 
   mutate(threshhold_label = str_c("City probability >= ", threshhold, "%", sep = ""),
          threshhold_label = fct_inorder(threshhold))
 
+threshhold_map %>% 
+  filter(flag_city == TRUE)
 
 threshhold_map %>% 
   filter(threshhold %in% c(.1, .3, .5)) %>% 
@@ -58,7 +68,8 @@ threshhold_map %>%
             #group_by(flag_city, threshhold) %>% 
             #summarize(), 
           ,aes(fill = flag_city), color = NA, alpha = .8) +
-  geom_sf(data = pgh_official_boundary, color = "yellow", fill = NA) +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "black", size = 1) +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "yellow", size = .3) +
   geom_sf(color = "black", fill = NA, size = 1) +
   geom_sf(color = "white", fill = NA, linetype = 2, size = 1) +
   facet_wrap(~threshhold_label) +
@@ -78,8 +89,8 @@ map <- threshhold_map %>%
             group_by(flag_city, threshhold, threshhold_label) %>% 
             summarize(),
           aes(fill = flag_city), color = NA, alpha = .8) +
-  geom_sf(data = pgh_official_boundary, color = "black", fill = NA, size = 1) +
-  geom_sf(data = pgh_official_boundary, color = "grey", fill = NA, linetype = 2, size = 1) +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "black", size = 4) +
+  geom_sf(data = pgh_official_boundary, alpha = 0, color = "yellow", size = 1) +
   geom_sf(color = "black", fill = NA, size = 1) +
   geom_sf(color = "white", fill = NA, linetype = 2, size = 1) +
   scale_fill_viridis_d() +
@@ -102,9 +113,9 @@ anim_save(filename = "output/animated_expanded_city_borders.gif", animation = an
 index_change <- threshhold_map %>% 
   ungroup() %>% 
   st_drop_geometry() %>% 
-  filter(.pred_city >= threshhold) %>% 
+  filter(mean_city >= threshhold) %>% 
   mutate(threshhold = as.numeric(threshhold)) %>% 
-  select(threshhold, GEOID, total_population_housed:housed_population_density_1k_per_km) %>% 
+  select(threshhold, GEOID, total_population_housed:housed_population_density_pop_per_square_km) %>% 
   pivot_longer(cols = -c(threshhold, GEOID), names_to = "variable") %>% 
   mutate(variable = str_c("mean_", variable, sep = "")) %>% 
   group_by(threshhold, variable) %>% 
@@ -117,12 +128,19 @@ index_change <- threshhold_map %>%
          pct_change = pct_change / 100) %>%
   ungroup()
 
+library(ggrepel)
 index_change %>% 
+  group_by(variable) %>% 
+  mutate(last_label = case_when(threshhold == min(threshhold) ~ variable,
+                                threshhold != min(threshhold) ~ NA_character_)) %>% 
+  ungroup() %>% 
   ggplot(aes(threshhold, pct_change, color = variable)) +
   geom_point(alpha = .3) +
   geom_smooth() +
+  geom_label_repel(aes(x = .08, label = last_label)) +
   scale_x_percent(trans = "reverse") +
   scale_y_percent() +
+  guides(color = FALSE) +
   labs(y = "Pct change indexed at first value") +
   theme_ipsum()
   
